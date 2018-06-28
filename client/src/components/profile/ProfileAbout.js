34fe1f96b46529f94d02abdc12aade44af556a75
sean <huangcrab@gmail.com>
2018-06-28 16:04:30 -0400

    expItem

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,8 +1,44 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import isEmpty from "../../validation/is-empty";
 
 class ProfileAbout extends Component {
   render() {
-    return <div>ProfileAbout</div>;
+    const { profile } = this.props;
+    //get first name
+    const firstName = profile.user.name.trim().split(" ")[0];
+
+    //get skills
+    const skills = profile.skills.map((skill, index) => {
+      return (
+        <div key={index} className="p-3">
+          <i className="fa fa-check" /> {skill}
+        </div>
+      );
+    });
+    return (
+      <div className="row">
+        <div className="col-md-12">
+          <div className="card card-body bg-light mb-3">
+            <h3 className="text-center text-info">{firstName}'s Bio</h3>
+            <p className="lead">
+              {isEmpty(profile.bio) ? (
+                <span>{firstName} did not put a bio here...</span>
+              ) : (
+                <span>{profile.bio}</span>
+              )}
+            </p>
+            <hr />
+            <h3 className="text-center text-info">Skill Set</h3>
+            <div className="row">
+              <div className="d-flex flex-wrap justify-content-center align-items-center">
+                {skills}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
