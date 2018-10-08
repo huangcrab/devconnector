98b8466bf7c001cc7478f589dd509b24c97a0853
sean <huangcrab@gmail.com>
2018-10-08 02:28:40 -0400

    fix devTool

--- client/src/store.js
+++ client/src/store.js
@@ -1,11 +1,18 @@
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
-// const initialState = {};
+const initialState = {};
 
-// const middleware = [thunk];
-
+const middleware = [thunk];
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+const store = createStore(
+  reducer,
+  /* preloadedState, */ composeEnhancers(applyMiddleware(...middleware))
+);
 // const store = createStore(
 //   rootReducer,
 //   initialState,
@@ -15,4 +22,4 @@
 //   )
 // );
 
-// export default store;
+export default store;
