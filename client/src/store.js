daad86f86b344f1c6f730b2ae9f275b20a57a573
sean <huangcrab@gmail.com>
2018-10-08 01:57:12 -0400

    fix devTool

--- client/src/store.js
+++ client/src/store.js
@@ -5,13 +5,17 @@ import rootReducer from "./reducers";
 const initialState = {};
 
 const middleware = [thunk];
-
+const devTools =
+  process.env.NODE_ENV === "developemnt"
+    ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+    : null;
 const store = createStore(
   rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devTools
   )
 );
 
