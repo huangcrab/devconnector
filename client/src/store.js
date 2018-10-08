fe63c5a224599cfda4b7b6f8e91bfbbe8a04b87c
sean <huangcrab@gmail.com>
2018-10-08 02:37:18 -0400

    fix devTool!

--- client/src/store.js
+++ client/src/store.js
@@ -1,42 +1,18 @@
-// import { createStore, applyMiddleware, compose } from "redux";
-// import thunk from "redux-thunk";
-// import rootReducer from "./reducers";
-
-// const initialState = {};
-
-// const middleware = [thunk];
-
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
-// export default store;
-import { createStore, compose, applyMiddleware } from "redux";
-import { createEpicMiddleware } from "redux-observable";
-import { createReducer } from "./reducer";
-import { epic } from "./epic";
-
-import { composeWithDevTools } from "redux-devtools-extension";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
-const configureStore = () => {
-  const epicMiddleware = createEpicMiddleware(epic);
-  const enhancers = composeEnhancers(applyMiddleware(epicMiddleware));
-  const store = createStore(
-    createReducer(),
-    initialState,
-    composeWithDevTools(
-      applyMiddleware(epicMiddleware)
-      // other store enhancers if any
-    )
-  );
-  return store;
-};
+const middleware = [thunk];
 
-export { configureStore };
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
