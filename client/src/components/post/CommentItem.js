406fdee7a79ce3141487c8cbf54a45afce39305e
sean <huangcrab@gmail.com>
2018-11-14 10:47:04 -0500

    redux devtool...

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import classnames from "classnames";
 import { Link } from "react-router-dom";
 import { deleteComment } from "../../actions/postActions";
 class CommentItem extends Component {
